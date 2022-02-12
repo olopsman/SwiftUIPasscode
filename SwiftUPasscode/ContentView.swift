412ffc7effa6efede52052c2225d443ad2d78037
Paulo Orquillo <olopsman@users.noreply.github.com>
2022-02-12 14:09:33 +1300

    Initial Commit

---
+++ SwiftUPasscode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUPasscode
+//
+//  Created by Paulo Orquillo on 12/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUPasscode/SwiftUPasscodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUPasscodeApp.swift
+//  SwiftUPasscode
+//
+//  Created by Paulo Orquillo on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUPasscodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
